1fe9d05924c68ceb0d7c9bd2455e58ea4cb45e39
Justin Fuller <justindanielfuller@gmail.com>
2017-02-28 09:01:11 -0500

    refactor(Server): Add tests and refactor several things for testing.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 // @flow
 
 import dotenv from 'dotenv';
-import cache from 'apicache';
-import compression from 'compression';
-import express from 'express';
-import helmet from 'helmet';
-import server from './server/server';
-import routes from './routes/routes';
+import init from './server/init';
 
 dotenv.config();
-const app = express();
-app.use(compression());
-app.use(helmet());
-app.all('/echo', cache.middleware('1 hour'), routes.echo);
-app.listen(process.env.PORT || 3000, server.listen);
+init();
---
+++ src/server/init.js
@@ -0,0 +1,16 @@
+import cache from 'apicache';
+import compression from 'compression';
+import express from 'express';
+import helmet from 'helmet';
+import server from './../server/server';
+import routes from './../routes/routes';
+
+function init() {
+  const app = express();
+  app.use(compression());
+  app.use(helmet());
+  app.all('/echo', cache.middleware('1 hour'), routes.echo);
+  app.listen(process.env.PORT || 3000, server.listen);
+}
+
+module.exports = init;
